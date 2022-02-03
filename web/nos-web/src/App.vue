5b97939b5895c3e73e16d96efd04920f1aca86d7
ccy <ccy1277@139.com>
2022-02-03 20:55:03 +0800

    更新项目目录

--- web/nos-web/src/App.vue
+++ web/nos-web/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <el-button>222</el-button>
+    我是app
     <router-view/>
   </div>
 </template>
@@ -14,11 +13,6 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
